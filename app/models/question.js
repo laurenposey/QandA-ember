dcc3edc381259531214236695415d9ecbc111adb
Lauren Posey <mrslaurenposey@gmail.com>
2016-08-25 13:40:33 -0700

    allow visitors to reply to questions

--- app/models/question.js
+++ app/models/question.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   content: DS.attr(),
   user: DS.attr(),
-  notes: DS.attr()
-
+  notes: DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
 });

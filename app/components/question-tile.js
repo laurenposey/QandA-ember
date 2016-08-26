0bf21fd67fecc0ce419eab9c7b7ccb7743c7b4e2
Lauren Posey <mrslaurenposey@gmail.com>
2016-08-26 13:51:45 -0700

    computed property display questionObject with notes, user, and notes

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  questionObject: Ember.computed('question.content', 'question.user','question.notes', function() {
+    return this.get('question.content') + 'Author: ' + this.get('question.content') + 'Notes:' + this.get('question.notes');
+  })
 });
